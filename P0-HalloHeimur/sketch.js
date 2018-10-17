e8d88c49de3e2e08ee82febaabd854e4d9790849
jonarnars <44092017+jonarnars@users.noreply.github.com>
2018-10-17 11:00:48 +0000

    up

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -1,1 +1,11 @@
-// Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(600,300);
+    background(300,0,50);
+    fill(100);
+    ellipse(250,100,400,100);
+    fill(10);
+    textSize(50);
+    text("Halló heimur",200,40);
+    rect(100,200,10,30)
+ellipse(100,250,90,10);
+}
