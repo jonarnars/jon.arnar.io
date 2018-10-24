23c38ae3a31d2227c51e5b45303b4d598c7ceb72
jonarnars <44092017+jonarnars@users.noreply.github.com>
2018-10-24 08:47:49 +0000

    and

---
+++ P2-Andlit/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+      createCanvas(900,900);
+      background(255,255,255);
+      rect(0,0,400,050);
+      fill(1);
+      rect(0,500,400,400);
+      fill(255);
+      rect(400,0,500,500);
+      fill(1);
+    }
