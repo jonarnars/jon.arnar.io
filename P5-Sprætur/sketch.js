7c9cedba91a7942f674b028cb0790bafbae69782
jonarnars <44092017+jonarnars@users.noreply.github.com>
2018-11-07 09:44:43 +0000

    skil ekki

--- P5-Sprætur/sketch.js
+++ P5-Sprætur/sketch.js
@@ -1,5 +1,7 @@
+Var isbjorn;
+
 function preload() {
-  mynd = loadImage('assets/isbjorn.jpg'); // https://www.google.is/search?q=polar+bear+cartoon&source=lnms&tbm=isch&sa=X&ved=0ahUKEwim8pG988HeAhVLPFAKHbtuC1UQ_AUIDigB&biw=1536&bih=723#imgdii=LUul2SHIxzlvYM:&imgrc=2sLNdphTpI8qaM:
+  isbjorn = loadImage('myndir/isbjorn.jpg'); // https://www.google.is/search?q=polar+bear+cartoon&source=lnms&tbm=isch&sa=X&ved=0ahUKEwim8pG988HeAhVLPFAKHbtuC1UQ_AUIDigB&biw=1536&bih=723#imgdii=LUul2SHIxzlvYM:&imgrc=2sLNdphTpI8qaM:
 }
 function setup() {
   createCanvas(700,450);
