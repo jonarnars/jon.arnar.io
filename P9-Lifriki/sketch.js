2bfd331837a2212739bd78a5bab8b1b33ebe5ee9
jonarnars <44092017+jonarnars@users.noreply.github.com>
2018-11-28 10:04:00 +0000

    mid

--- P9-Lifriki/sketch.js
+++ P9-Lifriki/sketch.js
@@ -1,42 +1,48 @@
-var bob1, bob2, bob3;
+var bobs = [];
 
 function setup() {
   createCanvas(600, 600);
-  // Búum til þrjá
-  bob1 = new Bob(200,400,100);
-  bob2 = new Bob(300,300,75);
-  bob3 = new Bob(400,300,150);
+  // Búum til hundrað zoog hluti.
+  for (var i = 0; i < 10; i = i+1){
+    bobs[i] = new Bob(random(200,400),random(100,400),random(50,200));
+  }
 }
 
 function draw() {
-	background(300,150,100);
+	background(200,150,0);
   fill(255,200,0);
-  ellipse(300,300,500,500);
-  // Ef hluturinn er nálgt miðjunni, þá hreyfir hann sig
-  bob1.move();
-  bob1.show();
-  bob2.move();
-  bob2.show();
-  bob3.move();
-  bob3.show();
+	ellipse(300,300,500,500);
+  // Teiknum og hreyfum alla Zoog kallana okkar
+  for (var i = 0; i < bobs.length; i = i+1){
+  	bobs[i].move();
+  	bobs[i].show();
+  }
 }
 
-// Klasinn
+function mousePressed(){
+  for (var i = bobs.length-1; i >= 0; i = i-1){
+    if(fjarl(bobs[i].x,bobs[i].y,mouseX,mouseY) < 50){
+		    bobs.splice(i,1);
+	  }
+  }
+}
+
+
 class Bob{
   constructor(x,y,bukur,litur) {
     this.x = x;
     this.y = y;
     this.bukur = bukur;
-    this.dir = random(0,4*PI);
-    this.speed = random(1);
-    this.bukurColor = color(100,100,100);
+		this.dir = random(0,2*PI);
+		this.speed = random(1,3);
+		this.bukurColor = color(100,100,100);
   }
 
-  // Teiknar geimveruna
-  show(){
+  // Teiknar geimveruna zoog
+   show(){
     ellipseMode(CENTER);
   //búkur
-  fill(236,36,94);
+  fill(this.bukurColor);
   ellipse(this.x,this.y,70,this.bukur)
  //haus
   fill(255);
@@ -51,7 +57,7 @@ class Bob{
 
 }
 
-  // Hreyfir
+  // Hreyfir zoog um skjáinn.
   // Hann minnkar sífelt hraðann, en velur svo slembistenfu
   // og fyglir henni á nyjum slbenum hraða.
 	move(){
@@ -62,7 +68,7 @@ class Bob{
 			this.dir = random(0,2*PI);
 			this.speed = random(1,5);
 		}
-      if (fjarlægð(this.x,this.y,width/2,height/2) > 250){
+      if (fjarl(this.x,this.y,width/2,height/2) > 250){
       	this.blink();
         this.dir = atan((this.y - height/2)/(this.x-width/2));
         if(this.x - width/2 > 0){
@@ -79,7 +85,7 @@ class Bob{
 
 // Skilar fjarlægðinni á milli punktanna
 // (x1,y1) og (x2,y2).
-function fjarlægð(x1,y1,x2,y2){
+function fjarl(x1,y1,x2,y2){
   var dist = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
   return dist;
 }
